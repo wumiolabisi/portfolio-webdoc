ea22aade44e98bb7564e789f1e67525e80b40090
unknown <wumi.olabisi@gmail.com>
2024-07-17 16:16:03 +0200

    Intégration de toutes les sections OK

--- src/app/presentation/page.js
+++ src/app/presentation/page.js
@@ -48,7 +48,7 @@ export default function Presentation() {
 
                 </div>
             </Diapositive>
-            <Diapositive >
+            <Diapositive classToAdd="hidden">
                 <div className="flex flex-col md:flex-row-reverse items-center justify-center w-full h-full">
                     <div className="flex flex-col w-full md:w-3/6 justify-center content-center items-center h-fit">
                         <Video height="video-h-long" src="/videos/video-omowumi-page-home.mp4" replacementImg="/wumi-section-presentation-1.jpg" />
@@ -64,6 +64,111 @@ export default function Presentation() {
                             <p className="text-white text-2xl">
                                 Je pouvais apprendre tout ce que je voulais sans quitter ma chambre ! Mieux que ça, je pouvais contribuer et publier ma vision du monde.
                             </p>
+                        </article>
+                    </div>
+                </div>
+            </Diapositive>
+            <Diapositive classToAdd="hidden">
+                <div className="flex flex-col md:flex-row justify-center w-full h-full">
+                    <div className="flex flex-col w-full md:w-3/6 justify-center items-center">
+                        <article className="text-wrap w-80">
+                            <p className="text-white text-2xl">
+                                Après le bac, j'ai voulu suivre un cursus qui me permettrait de mettre les mains dans le camboui rapidement.
+                            </p>
+                        </article>
+                    </div>
+                    <div className="flex flex-col w-full md:w-3/6 justify-center items-center">
+                        <article className="text-wrap w-80">
+                            <table className="text-white table-auto border-collapse">
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            2015
+                                        </td>
+                                        <td>
+                                            BTS Services Informatiques aux Organisations
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            2016
+                                        </td>
+                                        <td>
+                                            Licence Professionnelle Création et Développement numérique en ligne
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            2018
+                                        </td>
+                                        <td>
+                                            Certification RNCP XXX
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            2023
+                                        </td>
+                                        <td>
+                                            Certification RNCP XXX
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </article>
+                    </div>
+                </div>
+            </Diapositive>
+            <Diapositive>
+                <div className="flex flex-col md:flex-row items-center justify-center w-full h-full">
+                    <div className="flex flex-col w-full md:w-3/6 justify-center content-center items-center h-fit">
+                        <Video height="video-h-long" src="/videos/video-omowumi-page-home.mp4" replacementImg="/wumi-section-presentation-1.jpg" />
+                    </div>
+                    <div className="flex flex-col w-full md:w-3/6 justify-center items-center">
+                        <article className="text-wrap md:w-full w-96">
+                            <p className="text-white text-2xl">
+                                Grâce à mes expériences professionnelles, j'ai appris à être polyvalente et à travailler en équipe pour répondre aux besoins d'une entreprise.
+                            </p>
+                            <table className="text-white table-auto border-collapse">
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            2016-2018
+                                        </td>
+                                        <td>
+                                            <h3 className="font-bold">NUMBERLY</h3>
+                                            <p>Intégratrice e-mailing</p>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            2019-2021
+                                        </td>
+                                        <td>
+                                            <h3 className="font-bold">DEV IN MOTION (FREELANCE)</h3>
+                                            <p>Développeuse FullStack</p>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            2021-2023
+                                        </td>
+                                        <td>
+                                            <h3 className="font-bold">KOUNTAC/KROSKEL</h3>
+                                            <p>Développeuse WordPress</p>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            2023-2024
+                                        </td>
+                                        <td>
+                                            <h3 className="font-bold">SOLOCAL MARKETING SERVICESL</h3>
+                                            <p>Chargée de projets Webs/Webmaster</p>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </article>
                     </div>
                 </div>
